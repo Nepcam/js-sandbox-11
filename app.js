7245bd8c9fccf91e2b61664decee4646e4875e39
Nepcam <cameron.nepe@gmail.com>
2019-07-17 14:25:18 +1200

    create user, update user, delete user

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const http = new EasyHTTP;
 
 // Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// User Data
+const data = {
+  name: 'Cameron Nepe',
+  username: 'camnepe',
+  email: 'cnepe@gmail.com'
+}
+
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Update User
+// Create Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Delete Users
+http.delete('https://jsonplaceholder.typicode.com/users/2')
   .then(data => console.log(data))
   .catch(err => console.log(err));
-
